cb17632e368c8c6a03a142df3b2ed396f19070a3
Nathan Walters <nwalters512@gmail.com>
2016-03-30 11:12:43 -0500

    Updated sample application

--- sample/src/main/java/com/thebluealliance/spectrumsample/MainActivity.java
+++ sample/src/main/java/com/thebluealliance/spectrumsample/MainActivity.java
@@ -4,19 +4,43 @@ import com.thebluealliance.spectrum.SpectrumDialog;
 
 import android.graphics.Color;
 import android.support.annotation.ColorInt;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.open_dialog).setOnClickListener(this);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager();
+
+        mTabLayout = (TabLayout) findViewById(R.id.tabs);
+        mTabLayout.setupWithViewPager(mViewPager);
+        mTabLayout.setSelectedTabIndicatorColor(Color.WHITE);
+
+        //findViewById(R.id.open_dialog).setOnClickListener(this);
+    }
+
+    private void setupViewPager() {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFrag(new Fragment(), "Dialogs");
+        adapter.addFrag(new PreferencesDemoFragment(), "Preferences");
+        adapter.addFrag(new Fragment(), "Color Picker");
+        mViewPager.setAdapter(adapter);
     }
 
     @Override
@@ -29,7 +53,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         builder.setColorSelectedListener(new SpectrumDialog.ColorPickerListener() {
             @Override
             public void onColorPickerResult(int resultCode, @ColorInt int color) {
-                if(resultCode == SpectrumDialog.ColorPickerListener.POSITIVE) {
+                if (resultCode == SpectrumDialog.ColorPickerListener.POSITIVE) {
                     Toast.makeText(MainActivity.this, "Color selected! " + color, Toast.LENGTH_SHORT).show();
                 } else if (resultCode == SpectrumDialog.ColorPickerListener.NEGATIVE) {
                     Toast.makeText(MainActivity.this, "Color selection cancelled!", Toast.LENGTH_SHORT).show();
---
+++ sample/src/main/java/com/thebluealliance/spectrumsample/PreferencesDemoFragment.java
@@ -0,0 +1,33 @@
+package com.thebluealliance.spectrumsample;
+
+import com.thebluealliance.spectrum.SpectrumPreferenceCompat;
+
+import android.os.Bundle;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+public class PreferencesDemoFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            addPreferencesFromResource(R.xml.demo_preferences);
+        } catch (Exception e) {
+            e.printStackTrace();
+            e.getCause().printStackTrace();
+        }
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
+        // Do nothing?
+    }
+
+    @Override public void onDisplayPreferenceDialog(Preference preference) {
+        if (!SpectrumPreferenceCompat.onDisplayPreferenceDialog(preference, this)) {
+            super.onDisplayPreferenceDialog(preference);
+        }
+    }
+}
